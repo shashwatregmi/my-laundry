5d7d6a897a8a646442b4ad5cde5e21b9f10ab055
shashwatregmi <shashwat.regmi@icloud.com>
2020-06-26 00:28:37 -0700

    Created Project Files

---
+++ app/src/main/java/com/example/mylaundry/ui/calendar/CalendarFragment.java
@@ -0,0 +1,35 @@
+package com.example.mylaundry.ui.calendar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.mylaundry.R;
+
+public class CalendarFragment extends Fragment {
+
+    private CalendarViewModel calendarViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        calendarViewModel =
+                ViewModelProviders.of(this).get(CalendarViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        calendarViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/mylaundry/ui/calendar/CalendarViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mylaundry.ui.calendar;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class CalendarViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public CalendarViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is calendar fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}

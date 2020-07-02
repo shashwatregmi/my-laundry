ff3ba7dc424ae638c5cc59c3dbde0a805712e9f3
shashwatregmi <shashwat.regmi@icloud.com>
2020-07-02 14:22:15 -0700

    CalendarView into Bookings Activity

--- app/src/main/java/com/example/mylaundry/BookActivity.java
+++ app/src/main/java/com/example/mylaundry/BookActivity.java
@@ -1,12 +1,16 @@
 package com.example.mylaundry;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class BookActivity extends AppCompatActivity {
+
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,17 @@ public class BookActivity extends AppCompatActivity {
 
         TextView txtview = findViewById(R.id.textView);
         txtview.setText(title);
+
+        calendar = (CalendarView) findViewById(R.id.calendarView);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // we need to display the bookings for this day here by pulling from db..
+
+                TextView temp = findViewById(R.id.temptext);
+                temp.setText(String.valueOf(dayOfMonth) + "/" + String.valueOf(month) + "/" + String.valueOf(year));
+            }
+        });
     }
 }

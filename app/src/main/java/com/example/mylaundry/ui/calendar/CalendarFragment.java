44829226ca2059b4feda7974889be3f309450a2f
shashwatregmi <shashwat.regmi@icloud.com>
2020-07-03 16:42:34 -0700

    UI Updates + Started work on the Calendar View

--- app/src/main/java/com/example/mylaundry/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/mylaundry/ui/calendar/CalendarFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,8 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mylaundry.R;
 
+import java.util.ArrayList;
+
 public class CalendarFragment extends Fragment {
 
     private CalendarViewModel calendarViewModel;
@@ -22,14 +26,19 @@ public class CalendarFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         calendarViewModel =
                 ViewModelProviders.of(this).get(CalendarViewModel.class);
+        ArrayList<String> machineList = new ArrayList<>();
+
+        machineList.add("Washer #1");
+        machineList.add("Washer #2");
+
+        //TODO: pull data from DB and place into this array list..
+
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        calendarViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        Spinner spinner = (Spinner) root.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.spinnerlayout,
+                machineList);
+        adapter.setDropDownViewResource(R.layout.spinnerdropdownlayout);
+        spinner.setAdapter(adapter);
         return root;
     }
 }

a6811aa812f38360ea677b9b624483187a246a7e
shashwatregmi <shashwat.regmi@icloud.com>
2020-07-03 15:43:12 -0700

    TimePicker added for selecting time of booking

--- app/src/main/java/com/example/mylaundry/BookActivity.java
+++ app/src/main/java/com/example/mylaundry/BookActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
@@ -15,13 +17,17 @@ import android.widget.CalendarView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class BookActivity extends AppCompatActivity {
 
     CalendarView calendar;
     Button today;
+    Button book;
+    TimePickerDialog timePicker;
     String TODAY = "";
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -49,7 +55,7 @@ public class BookActivity extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                // we need to display the bookings for this day here by pulling from db..
+                // TODO: we need to display the bookings for this day here by pulling from db..
                 String temp = dayOfMonth + "/" + (month + 1) + "/" + year;
                 todayView.setText(temp);
             }
@@ -61,7 +67,24 @@ public class BookActivity extends AppCompatActivity {
             public void onClick(View v) {
                 calendar.setDate(System.currentTimeMillis());
                 todayView.setText(TODAY);
-                // need to update the bookings for today date on list now.
+                // TODO: need to update the bookings for today date on list now.
+            }
+        });
+
+        book = (Button) findViewById(R.id.bookbtn);
+        book.setOnClickListener(new View.OnClickListener() {
+            Calendar timeNow = Calendar.getInstance();
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePicker = new TimePickerDialog(BookActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String tempTime = String.valueOf(hourOfDay) + ":" + String.valueOf(minute);
+                        todayView.setText(tempTime);
+                        // TODO: need to do booking stuff here.
+                    }
+                }, timeNow.get(Calendar.HOUR_OF_DAY), timeNow.get(Calendar.MINUTE), false);
+                timePicker.show();
             }
         });
     }

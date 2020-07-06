0f552b1a2d2a6588b31e8dc7bfa5745ac7156611
shashwatregmi <shashwat.regmi@icloud.com>
2020-07-06 01:56:57 -0700

    UI Design of Settings/User Profile activity

--- app/src/main/java/com/example/mylaundry/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/mylaundry/ui/settings/SettingsFragment.java
@@ -23,13 +23,7 @@ public class SettingsFragment extends Fragment {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }

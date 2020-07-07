9b0bd5fc9d8a5d22ee437d7b86b81e583a878e17
shashwatregmi <shashwat.regmi@icloud.com>
2020-07-07 02:30:59 -0700

    Reposition/Relocation of various elements to fix some bugs

--- app/src/main/java/com/example/mylaundry/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/mylaundry/ui/settings/SettingsFragment.java
@@ -1,9 +1,12 @@
 package com.example.mylaundry.ui.settings;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,12 +15,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.mylaundry.BookActivity;
+import com.example.mylaundry.MainActivity;
 import com.example.mylaundry.R;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
-
+    private MainActivity main;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =

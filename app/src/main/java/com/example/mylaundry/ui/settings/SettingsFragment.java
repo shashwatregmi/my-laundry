2397b0237a51f9082029efc2a164e04521807b9e
shashwatregmi <shashwat.regmi@icloud.com>
2020-07-17 00:50:16 -0700

    Implemented function that allows users to view upcoming and previous bookings on the user settings fragment

--- app/src/main/java/com/example/mylaundry/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/mylaundry/ui/settings/SettingsFragment.java
@@ -5,30 +5,24 @@ import android.app.Activity;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.mylaundry.BookActivity;
 import com.example.mylaundry.BookItemAdapter;
 import com.example.mylaundry.Booking;
 import com.example.mylaundry.MainActivity;
 import com.example.mylaundry.R;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -43,25 +37,56 @@ public class SettingsFragment extends Fragment {
     private RecyclerView bookingRecyclerView;
     private RecyclerView.Adapter bookingAdapter;
     private RecyclerView.LayoutManager bookingLayoutManager;
+    private TabLayout bookingsTab;
     private ArrayList<Booking> dbBookingList = new ArrayList<>();
     String TODAY = "";
     FirebaseFirestore db;
-
-
     private SettingsViewModel settingsViewModel;
     private MainActivity main;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        final View root = inflater.inflate(R.layout.fragment_settings, container, false);
 
         @SuppressLint("SimpleDateFormat")
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date date = new Date();
         TODAY = dateFormat.format(date);
         db = FirebaseFirestore.getInstance();
+        bookingsTab = root.findViewById(R.id.tabLayout);
 
+        getUpcomingBookings(root);
+
+        bookingsTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0){
+                    getUpcomingBookings(root);
+                } else {
+                    getPreviousBookings(root);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ((Activity) root.getContext()).getWindow().setStatusBarColor(ContextCompat.getColor(root.getContext(), R.color.colorAccent));
+        }
+        return root;
+    }
+
+    private void getUpcomingBookings(View root){
+        dbBookingList.clear();
         db.collection("bookings").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
@@ -102,10 +127,49 @@ public class SettingsFragment extends Fragment {
 
         bookingRecyclerView.setLayoutManager(bookingLayoutManager);
         bookingRecyclerView.setAdapter(bookingAdapter);
+    }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ((Activity) root.getContext()).getWindow().setStatusBarColor(ContextCompat.getColor(root.getContext(), R.color.colorAccent));
-        }
-        return root;
+    private void getPreviousBookings(View root){
+        dbBookingList.clear();
+        db.collection("bookings").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if(!queryDocumentSnapshots.isEmpty()){
+                            List<DocumentSnapshot> tempBookings = queryDocumentSnapshots.getDocuments();
+                            Date bookingDate = null;
+                            Date todayDate = null;
+                            @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+                            try {
+                                todayDate = sdf.parse(TODAY);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+
+                            for (DocumentSnapshot d: tempBookings){
+                                Booking pulledBooking = d.toObject(Booking.class);
+                                try {
+                                    @SuppressLint("SimpleDateFormat") SimpleDateFormat bookFormat = new SimpleDateFormat("dd/MM/yyyy");
+                                    bookingDate = bookFormat.parse(pulledBooking.getDate());
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+
+                                if (bookingDate != null && (bookingDate.before(todayDate))){ // TODO: user check here after implementing user authent.
+                                    dbBookingList.add(pulledBooking);
+                                }                            }
+                            bookingAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+
+
+        bookingRecyclerView = root.findViewById(R.id.bookings);
+        bookingLayoutManager = new LinearLayoutManager(getContext());
+        bookingAdapter = new BookItemAdapter(dbBookingList);
+
+        bookingRecyclerView.setLayoutManager(bookingLayoutManager);
+        bookingRecyclerView.setAdapter(bookingAdapter);
     }
 }
